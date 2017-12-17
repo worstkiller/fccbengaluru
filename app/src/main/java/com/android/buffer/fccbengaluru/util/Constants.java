1931b75cbef6b31ff85514024668ca561a4d343a
dev.vikas <dev.vikas-at-665857559634>
2017-12-17 14:59:03 +0530

    feat: placeholder for key

--- app/src/main/java/com/android/buffer/fccbengaluru/util/Constants.java
+++ app/src/main/java/com/android/buffer/fccbengaluru/util/Constants.java
@@ -15,7 +15,7 @@ public class Constants {
     public static final String ENDPOINT = "https://graph.facebook.com/";
     public static final String GROUP_ID = "711261015663882";
     //one time token may expire soon
-    public static final String TOKEN = "EAAcVFZAi2BqQBAFVMsY5VJH5oXTreBiMR7ylQwWxZBZAXKemSsBUfCKizoNixAfStprdhFkUvoZBWu2645e0vZCl9T70rXfxFhqpyFn8qronrWo2HwTZADlbzhhjH3jcLSUaovTgRALuf0dBafKTCzXEaze6t3ytdB0ih7W2HECT3WdTxZArk2BLWJuyszRFR9GNJPZC8x9jqUFmV7pK5MRUlz2IsaycwQcmdmBF0ERmiVYYJFC1WaRz";
+    public static final String TOKEN = "your token";
     //static url to be shown
     public static final String FEED_URL = "https://www.facebook.com/groups/free.code.camp.bengaluru/";
     public static final int USER_ADMIN = 0;
